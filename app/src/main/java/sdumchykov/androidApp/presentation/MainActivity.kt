46fea1ffc043cd6f6cf8be0385d8b4730e2a1f08
Stanislav Dumchykov <mechanikea@gmail.com>
2023-12-09 15:06:44 +0200

    CHANGE Tiny changes in layouts activity_main.xml and activity_sign_up.xml

--- app/src/main/java/sdumchykov/androidApp/presentation/MainActivity.kt
+++ app/src/main/java/sdumchykov/androidApp/presentation/MainActivity.kt
@@ -1,28 +1,15 @@
 package sdumchykov.androidApp.presentation
 
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import sdumchykov.androidApp.R
-import sdumchykov.androidApp.databinding.ActivityMainBinding
-import sdumchykov.androidApp.presentation.base.BaseActivity
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-
-    private lateinit var navController: NavController
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.mainActivityNavHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
+        setContentView(R.layout.activity_main)
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-
 }
