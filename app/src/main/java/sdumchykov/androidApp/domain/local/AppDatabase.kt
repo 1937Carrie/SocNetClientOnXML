0c1e96645da8cf739d8fd7e3b434b9a25cdf69e5
Stanislav Dumchykov <mechanikea@gmail.com>
2023-12-09 14:26:31 +0200

    CHANGE Added parameter "exportSchema = false" to AppDatabase.kt

--- app/src/main/java/sdumchykov/androidApp/domain/local/AppDatabase.kt
+++ app/src/main/java/sdumchykov/androidApp/domain/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package sdumchykov.androidApp.domain.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
